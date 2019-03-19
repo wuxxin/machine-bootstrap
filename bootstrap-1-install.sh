b7a8cba9fd9c2bce888fd5bde2e603ef7af16331
Felix Erkinger <felix@erkinger.at>
2019-03-19 18:15:23 +0100

    fix unmount in bootstrap-1 and recovery unmount

--- bootstrap-1-install.sh
+++ bootstrap-1-install.sh
@@ -401,11 +401,19 @@ else
     echo "back in bootstrap-1-install"
 fi
 
-echo "unmount all"
-for i in run sys proc dev boot/efi boot/efi2 boot; do
+echo "unmount bind mounts"
+for i in run sys proc dev; do
     if mountpoint -q "/mnt/$i"; then umount -lf "/mnt/$i"; fi
 done
-
+echo "unmount legacy"
+for mountname in $(cat /mnt/etc/recovery/legacy.fstab | \
+    sed -r "s/^([^ ]+)( +)([^ ]+)( +)(.+)/\/mnt\3/g" | sort -r); do
+    umount -lf "$mountname" || echo "Warning: could not unmount legacy volume $mountname !"
+done
+echo "unmount boot, efi*"
+for i in boot/efi boot/efi2 boot; do
+    if mountpoint -q "/mnt/$i"; then umount -lf "/mnt/$i"; fi
+done
 sleep 1
-echo "export pool"
+echo "export rpool (unmount pool)"
 zpool export rpool
--- recovery/recovery-unmount.sh
+++ recovery/recovery-unmount.sh
@@ -8,9 +8,10 @@ for i in run sys proc dev; do
 done
 
 echo "unmount legacy"
-cat /mnt/etc/recovery/legacy.fstab | \
-    sed -r "s/^([^ ]+)( +)([^ ]+)( +)(.+)/\1\2\/mnt\3\4\5/g" > /tmp/legacy.fstab
-umount -a -T /tmp/legacy.fstab || echo "Warning: could not unmount all legacy volumes!"
+for mountname in $(cat /mnt/etc/recovery/legacy.fstab | \
+    sed -r "s/^([^ ]+)( +)([^ ]+)( +)(.+)/\/mnt\3/g" | sort -r); do
+    umount -lf "$mountname" || echo "Warning: could not unmount legacy volume $mountname !"
+done
 
 echo "unmount boot, efi*"
 for i in boot/efi boot/efi2 boot; do
@@ -18,7 +19,7 @@ for i in boot/efi boot/efi2 boot; do
 done
 
 echo "swap off"
-swapoff -a
+swapoff -a || true
 
 sleep 1
 echo "export rpool (unmount pool)"
