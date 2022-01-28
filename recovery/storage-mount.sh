2451c12ebef55733df2a17672c12f97fa7f4779a
Felix Erkinger <felix@erkinger.at>
2022-01-28 19:27:24 +0100

    cleanup args, make storage mount accept password

--- recovery/storage-mount.sh
+++ recovery/storage-mount.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -e
 
-self_path=$(dirname "$(readlink -e "$0")")
-force=false
-what=all
-password=""
-
-if test "$1" != "--yes"; then
+usage() {
     cat <<EOF
 Usage:
-    $0 --yes [--only-raid-luks|--without-raid-luks] [--password-from-stdin] [--force]
+    $0 --yes [--password-from-stdin] [--force]
 EOF
     exit 1
-fi
+}
+
+self_path=$(dirname "$(readlink -e "$0")")
+force="false"
+mount_options=""
+password=""
+
+if test "$1" != "--yes"; then usage; fi
 shift
-if test "$1" = "--only-raid-luks"; then what=until_luks; shift; fi
-if test "$1" = "--without-raid-luks"; then what=after_luks; shift; fi
-if test "$1" = "--password-from-stdin"; then password=$(cat -); shift; fi
+if test "$1" = "--password-from-stdin"; then
+    password=$(cat -)
+    mount_options="--password $password"
+    shift
+fi
 if test "$1" = "--force"; then force=true; shift; fi
 
 . "$self_path/bootstrap-library.sh"
@@ -27,22 +31,19 @@ if which cloud-init > /dev/null; then
     printf "\n"
 fi
 
-if test "$what" != "after_luks"; then
-    configure_nfs
-    zfs_packages="$(get_zfs_packages)"
-    echo "update sources, install $zfs_packages"
-    install_packages --refresh $zfs_packages
-    activate_mdadm
-    create_crypttab
-    activate_luks "$password"
-fi
-if test "$what" = "until_luks"; then exit 0; fi
+echo "update sources, install $zfs_packages"
+zfs_packages="$(get_zfs_packages)"
+configure_nfs
+install_packages --refresh $zfs_packages
 
+activate_mdadm
+create_crypttab
+activate_luks "$password"
 activate_lvm
-mount_root /mnt $force
+mount_root $mount_options /mnt $force
 mount_boot /mnt $force
 mount_efi /mnt
-mount_data /mnt/mnt $force
+mount_data $mount_options /mnt/mnt $force
 mount_bind_mounts /mnt
 
 cat << EOF
